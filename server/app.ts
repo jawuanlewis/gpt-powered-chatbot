105ca8b136371c64d96a2b651a0b8c95e80bca3c
Jawuan Lewis <jawuanlewis@users.noreply.github.com>
2025-06-21 09:12:36 -0700

    Deployment fix attempt

--- server/app.ts
+++ server/app.ts
@@ -106,7 +106,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 
   console.log('(Server) Registering catch-all route for SPA');
-  app.get('*', (req, res) => {
+  app.get(/^(?!\/api).*/, (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
   });
 }
